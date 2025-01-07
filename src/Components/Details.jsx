906f17c25133dc68b4bf5d72cad86bfe966f910d
krishna-85 <vaibhavchauhan658@gmail.com>
2025-01-08 00:50:48 +0530

    addded details

--- src/Components/Details.jsx
+++ src/Components/Details.jsx
@@ -1,16 +1,40 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
+import React, { useContext, useEffect } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import { productContext } from './utils/Context'
+import axios from '../Components/utils/axios'
+import { useState } from 'react'
+import Loading from './Loading'
+ 
 const Details = () => {
+    const [products, setproducts] = useState(null)
+    const {id} = useParams();
+    console.log(id)
+    const getSingleproduct = async ()=>{
+        try {
+           const {data} = await axios.get(`/products/${id}`)
+           setproducts(data);
+        } catch (error) {
+            console.log(error);
+            
+        }
+    }
+
+    useEffect(()=>{
+getSingleproduct()
+    },[])
+
+    if (!products || products.length === 0) {
+        return <Loading />;
+      }
   return (
     <div className='w-[70%] flex gap-10 items-center    h-full   m-auto p-[10%]'>
-<img className='h-[80%] w-[40%] object-cover' src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="" />
+<img className='h-[80%] w-[40%] object-cover' src={`${products.image}`} alt="" />
 
 <div className="font-semibold w-[90%]  content">
-    <h1 className='mb-2 text-3xl font-semibold'>Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</h1>
-    <h1 className='text-xl mb-2 text-zinc-400'>Men's clothing</h1>
+    <h1 className='mb-2 text-3xl font-semibold'>{products.title}</h1>
+    <h1 className='text-xl mb-2 text-zinc-400'>{products.category}</h1>
     <h2 className='text-red-400 mb-2'>₹109.95</h2>
-    <p className='mb-10'>Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday</p>
+    <p className='mb-10'>{products.description}</p>
     
     <Link className=' mr-5 py-2 px-5 border rounded border-blue-300'>Edit</Link>
     <Link className='py-2 px-5 border rounded border-red-300'>Delete</Link>
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
     <Nav/>
     <div className="w-[85%] p-10 flex flex-wrap overflow-x-hidden overflow-y-auto  pt-[5%]  h-full">
     {products.map((products)=>{
-      return  <Link to="/details/1" className="card flex mr-3 mb-3 p-3 justify-center items-center flex-col w-[18%] h-[30vh] border shadow rounded">
+      return  <Link to={`/details/${products.id}`} className="card flex mr-3 mb-3 p-3 justify-center items-center flex-col w-[18%] h-[30vh] border shadow rounded">
         <div className="hover:scale-110 w-full mb-5 h-[80%] bg-contain bg-center bg-no-repeat"
           style={{
             backgroundImage: `url(${products.image})`
